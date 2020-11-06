2e3118c0d17bcaf65ff8062dd48e84db8961e7fe
Avkash <avkaashsharma89@gmail.com>
2020-11-06 02:41:57 -0500

    Create Register model

--- Client/src/main/java/Requests/RegisterMessage.java
+++ Client/src/main/java/Requests/RegisterMessage.java
@@ -1,8 +1,42 @@
 package Requests;
 
-public class RegisterMessage extends Message{
+import java.net.InetSocketAddress;
 
-    public RegisterMessage() {
+/*
+Register Request
+- Request #
+- Unique name
+- IP Address 
+- Socket #
+*/
+
+public class RegisterMessage extends Message {
+
+    InetSocketAddress clientSocketAddress;
+    String clientName;
+
+    public RegisterMessage(String clientName, InetSocketAddress clientSocketAddress) {
         super(RequestType.REGISTER);
+        this.clientName = clientName;
+        this.clientSocketAddress = clientSocketAddress;
     }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public String toString() {
+        return RequestType.REGISTER + " " + clientName + " " + clientSocketAddress;
+    }
+
+    // TEST CODE
+    // public static void main(String[] args) throws UnknownHostException {
+    //     RegisterMessage rm = new RegisterMessage("rrr", new InetSocketAddress(InetAddress.getLocalHost(), 6000));
+
+    //     System.out.println(rm.clientSocketAddress.getPort());
+
+    // }
+
+
+
 }
